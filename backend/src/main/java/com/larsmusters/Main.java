02206b7fb25b54d1c7af76b52a9bef2d37c9dda8
larsmusters <lars.musters@hotmail.com>
2023-05-07 23:04:37 +0200

    setting up for building backend

--- backend/src/main/java/com/larsmusters/Main.java
+++ backend/src/main/java/com/larsmusters/Main.java
@@ -1,13 +1,14 @@
-package com.example.backend;
+package com.larsmusters;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
-public class BackendApplication {
+public class Main {
 
     public static void main(String[] args) {
-        SpringApplication.run(BackendApplication.class, args);
+        SpringApplication.run(Main.class, args);
     }
 
 }
--- backend/src/test/java/com/larsmusters/MainTests.java
+++ backend/src/test/java/com/larsmusters/MainTests.java
@@ -1,10 +1,10 @@
-package com.example.backend;
+package com.larsmusters;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class BackendApplicationTests {
+class MainTests {
 
     @Test
     void contextLoads() {
